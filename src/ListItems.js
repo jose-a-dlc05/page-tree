4d777c9fb0ae05cf09fc3280a5af01eeb39fe3f2
Jose De La Cruz <jose.delcruz0490@icloud.com>
2022-11-30 20:09:12 -0500

    Added collapsible list

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 export default function ListItems({ data: listItemData, key }) {
+	const [collapse, setCollapse] = useState(true);
 	return (
-		<li key={key}>
+		<li key={key} onClick={() => setCollapse(!collapse)}>
 			{listItemData?.name}
 			<ul>
 				{listItemData.children?.map((item, idx) => {
-					return <ListItems key={idx} data={item} />;
+					return collapse && <ListItems key={idx} data={item} />;
 				})}
 			</ul>
 		</li>
